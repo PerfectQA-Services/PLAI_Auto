9676f7b992adbae2afc29e4c1619ecf3cc202e2b
QA <qa@apples-MacBook-Pro-3.local>
2023-01-05 18:15:41 +0530

    Adding new test of SettingTab

--- src/test/java/Tests/SettingTest.java
+++ src/test/java/Tests/SettingTest.java
@@ -578,4 +578,370 @@ public class SettingTest extends Configuration {
         loginPage.Get_Toast_Alert("Message on valid email id.");
 
     }
+    //379
+    @Test
+    public void Verify_the_Legal_field() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Legal_Label();
+        settingpage.Verify_Legal_page_Header();
+
+    }
+    //380
+    @Test
+    public void Verify_the_field_display_on_LEGAL_screen() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Legal_Label();
+        settingpage.Verify_Legal_Page_Element();
+
+    }
+    //381
+    @Test
+    public void Verify_the_Terms_of_usefield() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Legal_Label();
+        settingpage.Verify_Terms_Of_Use_Page_Element();
+
+    }
+    //382
+    @Test
+    public void Verify_the_Back_icon_of_TERMS_OF_USE_screen() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SignUpPage signup=new SignUpPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Legal_Label();
+        settingpage.Verify_Terms_Of_Use_Page_Element();
+        signup.click_on_back_button();
+        settingpage.Verify_Legal_page_Header();
+
+    }
+    //383
+    @Test
+    public void Verify_the_Privacy_Policy_field() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Legal_Label();
+        settingpage.Verify_Privacy_Policy_Page_Element();
+
+    }
+    //384
+    @Test
+    public void Verify_the_Back_icon_of_PRIVACY_POLICY_screen() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SignUpPage signup=new SignUpPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Legal_Label();
+        settingpage.Verify_Privacy_Policy_Page_Element();
+        signup.click_on_back_button();
+        settingpage.Verify_Legal_page_Header();
+
+    }
+    //385
+    @Test
+    public void Verify_the_Buyer_And_Seller_Terms_field() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Legal_Label();
+        settingpage.Verify_Buyer_And_Seller_Page_Element();
+
+    }
+    //386
+    @Test
+    public void Verify_the_Back_icon_of_BUYER_And_SELLER_TERMS_screen() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SignUpPage signup=new SignUpPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Legal_Label();
+        settingpage.Verify_Buyer_And_Seller_Page_Element();
+        signup.click_on_back_button();
+        settingpage.Verify_Legal_page_Header();
+
+    }
+    //387
+    @Test
+    public void Verify_the_Delete_Account_field() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Legal_Label();
+        settingpage.Verify_Delete_Account_Page_Element();
+
+    }
+    //388
+    @Test
+    public void Verify_the_Dismiss_button_of_confirmation_popup() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Legal_Label();
+        settingpage.Click_On_Dismiss_Button_And_Verify_Result();
+
+    }
+    //389
+    @Test
+    public void Verify_the_Confirm_button_of_confirmation_popup() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Legal_Label();
+        settingpage.Click_On_Confirm_Button_And_Verify_Result();
+
+    }
+    //390
+    @Test
+    public void Verify_the_Back_icon_of_LEGAL_screen() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        SignUpPage signup=new SignUpPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Legal_Label();
+        signup.click_on_back_button();
+        settingpage.Verify_Setting_Header();
+
+    }
+    //391
+    @Test
+    public void Verify_the_Change_Password_field() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        SignUpPage signup=new SignUpPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Change_Password_Label();
+        settingpage.Verify_Change_Password_page_Header();
+
+    }
+    //392
+    @Test
+    public void Verify_the_field_display_on_CHANGE_PASSWORD_field() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        SignUpPage signup=new SignUpPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Change_Password_Label();
+        settingpage.Verify_Change_Password_Page_Element();
+
+    }
+    //393
+    @Test
+    public void Verify_the_UPDATE_PASSWORD_button_without_entering_data_in_mandatory_field() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        SignUpPage signup=new SignUpPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Change_Password_Label();
+        settingpage.Click_On_Update_Password_Button();
+        loginPage.Get_Toast_Alert("Error message while click on update password button without entering data.");
+
+    }
+    //394
+    @Test
+    public void Verify_the_Eye_icon_of_Current_Password_field() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        SignUpPage signup=new SignUpPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Change_Password_Label();
+        settingpage.Enter_Current_password_And_Verify_Functionality_Of_Eye_Icon();
+
+    }
+    //395
+    @Test
+    public void Verify_the_New_Password_field_by_entering_7_characters() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        SignUpPage signup=new SignUpPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Change_Password_Label();
+        settingpage.Enter_New_7_char_password_And_Verify_text_In_Red_Colour();
+
+    }
+    //396
+    @Test
+    public void Verify_the_New_Password_field_by_entering_password_which_contain_8_character_including_uppercase_letter_and_special_symbol() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        SignUpPage signup=new SignUpPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Change_Password_Label();
+        settingpage.Enter_New_8_char_Valid_password_And_Verify_text_In_Green_Colour();
+
+    }
+    //397
+    @Test
+    public void Verify_the_Eye_icon_functionality_for_New_Password_field() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        SignUpPage signup=new SignUpPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Change_Password_Label();
+        settingpage.Enter_New_password_And_Verify_Functionality_Of_Eye_Icon();
+
+    }
+    //398
+    @Test
+    public void Verify_the_Confirm_Password_field_by_entering_different_password_than_New_Password_field() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        SignUpPage signup=new SignUpPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Change_Password_Label();
+        settingpage.Enter_Current_password_And_Verify_Functionality_Of_Eye_Icon();
+        settingpage.Enter_New_8_char_Valid_password_And_Verify_text_In_Green_Colour();
+        settingpage.Enter_wrong_Confirm_password();
+        settingpage.Click_On_Update_Password_Button();
+        loginPage.Get_Toast_Alert("Error message while enter wrong confirm password.");
+
+    }
+    //399
+    @Test
+    public void Verify_the_Confirm_Password_field_by_entering_same_password_than_New_Password_field() throws InterruptedException, IOException, ParseException {
+
+        LaunchAppPage launchapp = new LaunchAppPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        SettingPage settingpage=new SettingPage(driver);
+        SignUpPage signup=new SignUpPage(driver);
+        launchapp.Verify_Application_Launched();
+        launchapp.click_on_skip_button();
+        launchapp.click_on_next_button();
+        loginPage.Login_with_valid_Credentials();
+        settingpage.Click_On_Setting_Icon();
+        settingpage.Click_On_Change_Password_Label();
+        settingpage.Enter_Current_password_And_Verify_Functionality_Of_Eye_Icon();
+        settingpage.Enter_New_8_char_Valid_password_And_Verify_text_In_Green_Colour();
+        settingpage.Enter_Same_New_and_Confirm_password();
+        settingpage.Click_On_Update_Password_Button();
+        settingpage.Verify_Setting_Header();
+
+    }
+
 }
